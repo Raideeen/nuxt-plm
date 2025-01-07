b6ec4d3cd06dcfa0ea8cc6d70dfb3f13d4ee278a
raideeen@proton.me <raideeen@proton.me>
2025-01-08 00:49:46 +0100

    feat: try out Lucide Icon library for vue

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Nuxt Lucide Icons</h1>
+    <LucideRocket />
+    <LucideFolderGit2 color="red" />
+    <LucideGithub :size="32" />
   </div>
 </template>
